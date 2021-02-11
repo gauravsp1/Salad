8c1cf77ad9fba5484cc6e04df503c2003144f47d
Gaurav Pawar <gauravspawar9624@gmail.com>
2021-02-11 22:23:42 +0530

    testing

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -7,8 +7,8 @@ function NavBar(){
      <div id="navBar" className="navBar ">
         <img className="navImg " src="./images/logo.jpg"/>
         <ul className="navList">
-            <li className="navItem"><NavLink to="https://gauravsp1.github.io/salad/">HOME</NavLink></li>
-            <li className="navItem"><NavLink to="https://gauravsp1.github.io/salad/menu">MENU</NavLink></li>
+            <li className="navItem"><NavLink to="/">HOME</NavLink></li>
+            <li className="navItem"><NavLink to="/menu">MENU</NavLink></li>
             <li className="navItem"><a href="#location">LOCATION</a></li>
             <li className="navItem"><a href="#plan">SIGN UP</a></li>
         </ul>
